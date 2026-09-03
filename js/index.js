6b4f4758e47434731006aa32fefd97fdfb552c3e
Kenny Adu Antwi <knkadu_antwi001@st.ug.edu.gh>
2026-09-02 20:37:28 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -26,3 +26,35 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+let messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let usersName = event.target.usersName.value;
+    let usersEmail = event.target.usersEmail.value;
+    let usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName, usersEmail, usersMessage);
+
+    let messageSection = document.querySelector("#messages");
+    let messageList = messageSection.querySelector("ul");
+
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+        <span>${usersMessage}</span>
+    `;
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function() {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
